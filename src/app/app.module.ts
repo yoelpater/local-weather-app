85cfb244b2d09089a926f495d40809409384bcf0
Yoelp <51400150+yoelpater@users.noreply.github.com>
2022-01-28 12:46:09 +0800

    add new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNewComponentComponent
   ],
   imports: [
     BrowserModule
