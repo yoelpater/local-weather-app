bca1bd6f5bc2359231982d6db03509fe00a933af
Yoelp <51400150+yoelpater@users.noreply.github.com>
2022-02-08 17:14:53 +0800

    fix lint cypres jest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,9 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
 import { CurrentWeatherComponent } from './current-weather/current-weather.component'
-import { MyNewComponentComponent } from './my-new-component/my-new-component.component'
 
 @NgModule({
-  declarations: [AppComponent, MyNewComponentComponent, CurrentWeatherComponent],
+  declarations: [AppComponent, CurrentWeatherComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent, CurrentWeatherComponent],
