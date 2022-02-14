908e37cd0ef04ec80c684fcecdc34a2263784607
Yoelp <51400150+yoelpater@users.noreply.github.com>
2022-02-14 14:49:12 +0800

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   appId: '38dcff8f4ba8b49d1fc04cc5566720ee',
-  baseUrl: 'https://',
+  baseUrl: 'http://',
   username: 'yoelpater',
   geonamesApi: 'api',
 }
