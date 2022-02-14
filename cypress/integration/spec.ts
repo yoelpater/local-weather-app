5dfdb72887bbb501b354a790f4334079a8d227a8
Yoelp <51400150+yoelpater@users.noreply.github.com>
2022-02-14 14:11:41 +0800

    add respective e2e test for search field

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -6,3 +6,57 @@ describe('Init Test', () => {
     cy.contains('Your city, your forecast, right now!')
   })
 })
+
+describe('Test search field if inputs are invalid', () => {
+  it('Shows error message when only a single character inserted to search field', () => {
+    cy.visit('/')
+    cy.get('input').first().focus()
+    cy.get('input').first().type('H')
+    cy.get('input').first().blur()
+    // Type more than one character to search
+    cy.contains('Type more than one character to search')
+  })
+
+  it('Shows error message when no character inserted to search field', () => {
+    cy.visit('/')
+    cy.get('input').first().focus()
+    cy.get('input').first().blur()
+    // Type more than one character to search
+    cy.contains('Type more than one character to search')
+  })
+})
+
+
+describe('Test search field when inputs are valid', () => {
+  it('Shows Greater London when searched by uk zip code (SW1W 0NY)', () => {
+    cy.visit('/')
+    cy.get('input').first().focus()
+    cy.get('input').first().type('SW1W 0NY')
+    cy.get('input').first().blur()
+    cy.contains('Greater London, GB')
+  })
+  it('Shows US when searched by us zip code (90210)', () => {
+    cy.visit('/')
+    cy.get('input').first().focus()
+    cy.get('input').first().type('90210')
+    cy.get('input').first().blur()
+    cy.contains('Beverly Hills, US')
+  })
+
+  it('Shows Paris weather when searched by its city name', () => {
+    cy.visit('/')
+    cy.get('input').first().focus()
+    cy.get('input').first().type('paris')
+    cy.get('input').first().blur()
+    cy.contains('Paris')
+  })
+
+  it('Shows results when searched by its city name and country code', () => {
+    cy.visit('/')
+    cy.get('input').first().focus()
+    cy.get('input').first().type('jakarta,id')
+    cy.get('input').first().blur()
+    cy.contains('Jakarta, ID')
+  })
+
+})
